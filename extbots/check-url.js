1e10ade1f39d79ac5e1ed888fd6c0a1ed9c31146
Wellers20 <priiipri324@gmail.com>
2026-03-11 20:47:15 +0300

    Delete extbots directory

--- extbots/check-url.js
+++
@@ -1,7 +0,0 @@
-import https from 'https';
-
-https.get('https://github.com/Wellers20/OcenIVATel/releases/download/1.0.0/dog+3d+model.obj', (res) => {
-  console.log('Status:', res.statusCode);
-  console.log('Headers:', res.headers);
-  console.log('Location:', res.headers.location);
-});
--- extbots/check-url3.js
+++
@@ -1,8 +0,0 @@
-import https from 'https';
-
-const url = 'https://corsproxy.io/?' + encodeURIComponent('https://github.com/Wellers20/OcenIVATel/releases/download/1.0.0/dog+3d+model.obj');
-
-https.get(url, (res) => {
-  console.log('Status:', res.statusCode);
-  console.log('Headers:', res.headers);
-});
